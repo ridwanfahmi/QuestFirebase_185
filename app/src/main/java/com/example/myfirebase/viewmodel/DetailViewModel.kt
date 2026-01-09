1c2c804df6271fe02a0f212b98ac24ee56a60e21
ridwanfahmi <ridwanfahmidina12@gmail.com>
2026-01-09 08:38:15 +0700

    menambahkan StatusUIDetail pada DetailViewModel

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.myfirebase.viewmodel
 
+import com.example.myfirebase.modeldata.Siswa
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
